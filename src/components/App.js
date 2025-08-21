e8365272fa5c0f2190537105409f6d01f2a2e9e8
Yogender <yashmittal92002@gmail.com>
2025-08-21 11:32:33 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[display,setDisplay]=useState(false)
+
+  const handleParagraph=()=>{
+    setDisplay(prev=>!prev)
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+       <button id="click" onClick={handleParagraph}>button</button>
+       { display &&(
+       <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+       }
     </div>
   );
 }
